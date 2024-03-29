f2337f65d736f2fdcabffd9113a7b075fe72a238
Birdmate <165241828+Birdmate@users.noreply.github.com>
2024-03-29 13:58:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let flipTimer; // 裏返し中タイマー
 let fstCard = false; // ゲームが開始されているかどうかを示すフラグ
 let fstCardArea; // 1枚目の場所
 let counter = 0; // そろえた枚数
+let gameStarted = false; // ゲームが開始されたかどうかを示すフラグ
 
 let cardName = [
     "logo", "ezokogera", "kogera", "miyakekogera",
@@ -44,7 +45,6 @@ function shuffle(cards) {
 }
 
 window.onload = () => {
-    initCards();
     let start = document.getElementById('start');
     let stop = document.getElementById('stop');
     start.addEventListener('click', startStopGame);
@@ -57,90 +57,88 @@ function startStopGame(event) {
     let button = event.target;
     if (button.id === 'start') {
         startGame();
-        button.textContent = 'リセット';
-        button.id = 'reset';
-        let stopButton = document.getElementById('stop');
-        stopButton.textContent = 'ストップ';
     } else if (button.id === 'stop') {
         stopGame();
-        button.textContent = '再スタート';
-        button.id = 'restart';
-        let startButton = document.getElementById('start');
-        startButton.textContent = 'スタート';
     } else if (button.id === 'restart') {
         restartGame();
-        button.textContent = 'ストップ';
-        button.id = 'stop';
-        let startButton = document.getElementById('start');
-        startButton.textContent = 'リセット';
     } else if (button.id === 'reset') {
         resetGame();
-        button.textContent = 'スタート';
-        button.id = 'start';
-        let stopButton = document.getElementById('stop');
-        stopButton.textContent = 'ストップ';
     }
 }
 
 function startGame() {
-    initCards();
-    startTime = new Date();
-    startTimer();
-    let startButton = document.getElementById('start');
-    startButton.textContent = 'リセット';
-    startButton.id = 'reset'; // IDを 'reset' に変更
-    let stopButton = document.getElementById('stop');
-    stopButton.textContent = 'ストップ';
+    if (!gameStarted) {
+        initCards();
+        startTime = new Date();
+        startTimer();
+        let startButton = document.getElementById('start');
+        startButton.textContent = 'リセット';
+        startButton.id = 'reset'; // IDを 'reset' に変更
+        let stopButton = document.getElementById('stop');
+        stopButton.textContent = 'ストップ';
+        gameStarted = true;
+    }
 }
 
 function stopGame() {
-    clearInterval(timer);
-    let stopButton = document.getElementById('stop');
-    stopButton.textContent = '再スタート';
-    stopButton.id = 'restart'; // IDを 'restart' に変更
+    if (gameStarted) {
+        clearInterval(timer);
+        let stopButton = document.getElementById('stop');
+        stopButton.textContent = '再スタート';
+        stopButton.id = 'restart'; // IDを 'restart' に変更
+    }
 }
 
 function restartGame() {
-    initCards();
-    // startTime を更新せず、以前の開始時刻から再開する
-    startTimer();
+    if (gameStarted) {
+        initCards();
+        // startTime を更新せず、以前の開始時刻から再開する
+        startTimer();
+        let startButton = document.getElementById('start');
+        startButton.textContent = 'リセット';
+        startButton.id = 'reset'; // IDを 'reset' に変更
+        let stopButton = document.getElementById('stop');
+        stopButton.textContent = 'ストップ';
+    }
 }
 
 function flip(crnt) {
-    let div = crnt.target;
-    if (flipTimer) return;
-    if (div.innerHTML == '') {
-        div.className = 'card';
-        div.innerHTML = cardTag[div.number];
-    } else {
-        return;
-    }
-    if (fstCard) {
-        fstCardArea = div;
-        fstCard = false;
-    } else {
-        if (fstCardArea.number == div.number) {
-            counter++;
-            flipTimer = setTimeout(function () {
-                div.className = 'card finish';
-                fstCardArea.className = 'card finish';
-                flipTimer = NaN;
-                if (counter == 10) {
-                    clearInterval(timer);
-                    finishGame();
-                }
-            }, 500);
+    if (gameStarted) {
+        let div = crnt.target;
+        if (flipTimer) return;
+        if (div.innerHTML == '') {
+            div.className = 'card';
+            div.innerHTML = cardTag[div.number];
         } else {
-            flipTimer = setTimeout(function () {
-                div.className = 'card back';
-                div.innerHTML = '';
-                fstCardArea.className = 'card back';
-                fstCardArea.innerHTML = '';
-                fstCardArea = null;
-                flipTimer = NaN;
-            }, 500);
+            return;
         }
-        fstCard = true;
+        if (fstCard) {
+            fstCardArea = div;
+            fstCard = false;
+        } else {
+            if (fstCardArea.number == div.number) {
+                counter++;
+                flipTimer = setTimeout(function () {
+                    div.className = 'card finish';
+                    fstCardArea.className = 'card finish';
+                    flipTimer = NaN;
+                    if (counter == 10) {
+                        clearInterval(timer);
+                        finishGame();
+                    }
+                }, 500);
+            } else {
+                flipTimer = setTimeout(function () {
+                    div.className = 'card back';
+                    div.innerHTML = '';
+                    fstCardArea.className = 'card back';
+                    fstCardArea.innerHTML = '';
+                    fstCardArea = null;
+                    flipTimer = NaN;
+                }, 500);
+            }
+            fstCard = true;
+        }
     }
 }
 
@@ -169,6 +167,7 @@ function resetGame() {
     let result = document.getElementById('result');
     result.innerHTML = '';
     counter = 0;
+    gameStarted = false;
 }
 
 function finishGame() {
